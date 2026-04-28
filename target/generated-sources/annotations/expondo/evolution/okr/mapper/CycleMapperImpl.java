706e205b71b7cb27d15971f295076dc7a2b6b67d
glaures <guido@laures.de>
2026-04-28 13:11:55 +0200

    Jira integration Phase 1

--- target/generated-sources/annotations/expondo/evolution/okr/mapper/CycleMapperImpl.java
+++ target/generated-sources/annotations/expondo/evolution/okr/mapper/CycleMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-14T16:12:30+0100",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+    date = "2026-04-28T13:10:00+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.10 (Amazon.com Inc.)"
 )
 @Component
 public class CycleMapperImpl implements CycleMapper {
